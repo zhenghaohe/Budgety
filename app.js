a1e0e8762e900076cc3fdef505dcd84a20b0e519
zhenghao <iron4wine@outlook.com>
2018-05-14 11:23:20 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var budgetController = (function() {
+  //
+})();
+
+
+var UIController = (function() {
+  //
+})();
+
+var controller = (function(budgetCtrl,UICtrl){
+  //
+})(budgetController,UIController);
