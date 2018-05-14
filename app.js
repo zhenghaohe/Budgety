1d9a28dded807e4aec05622ab6996ea01ca7d5c7
zhenghao <iron4wine@outlook.com>
2018-05-14 12:38:48 -0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
-  //
-})();
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
+})();
 
 // UI CONTROLLER
 var UIController = (function() {
@@ -15,23 +35,28 @@ var UIController = (function() {
   return {
     getInput() {
       return{
-        type : document.querySelector(DOMstrings.inputType).value, //will be either 'inc' or 'exp'
+        type : document.querySelector(DOMstrings.inputType).value,
         description : document.querySelector(DOMstrings.inputDescription).value,
         value : document.querySelector(DOMstrings.inputValue).value
       };
     },
     getDOMStrings() {
       return DOMstrings;
-    }
+    },
   }})();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl,UICtrl){
-  var DOM = UICtrl.getDOMStrings();
+
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMStrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress',ctrlAddItem);
+  }
+
   var ctrlAddItem = function() {
     //Get the filled input data
     var input = UICtrl.getInput();
-    console.log(input);
     //Add the item to the budget CONTROLLER
 
     //Add the item to the UI
@@ -41,8 +66,12 @@ var controller = (function(budgetCtrl,UICtrl){
     //Display the budget on the UI
   }
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress',ctrlAddItem);
-
+  return {
+    init() {
+      console.log('App started');
+      setupEventListeners();
+    }
+  }
 })(budgetController,UIController);
+
+controller.init();
