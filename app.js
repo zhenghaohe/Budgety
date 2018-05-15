573fb5183a5ee5d99cbf88781369daf873d8842b
zhenghao <iron4wine@outlook.com>
2018-05-14 18:32:29 -0700

    update

--- app.js
+++ app.js
@@ -154,7 +154,13 @@ var UIController = (function() {
       document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
       document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
       document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
-      document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else{
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+
+      }
     }
 
 
@@ -197,6 +203,12 @@ var controller = (function(budgetCtrl,UICtrl){
     init() {
       console.log('App started');
       setupEventListeners();
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
     }
   }
 })(budgetController,UIController);
