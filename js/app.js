c3bb07a6a95451516f308f395ea7f56b5003223b
zhenghao <iron4wine@outlook.com>
2018-05-19 18:37:43 -0700

    fix bugs

--- js/app.js
+++ js/app.js
@@ -93,7 +93,7 @@ const budgetController = (() => {
       calculateTotal('inc');
       data.budget = data.totals.inc - data.totals.exp;
 
-      if (data.totals.in > 0) {
+      if (data.totals.inc > 0) {
         data.percentage = Math.round(data.totals.exp / data.totals.inc * 100);
       } else {
         data.percentage = -1;
@@ -259,7 +259,7 @@ const AppController = ((budgetCtrl, UICtrl) => {
   };
   const setupEventListeners = () => {
     addButton.addEventListener('click', ctrlAddItem);
-    document.addEventListener('keypress', (e) => {
+    document.addEventListener('keypress', e => {
       if (e.keyCode === 13) ctrlAddItem();
     });
     budgetContainer.addEventListener('click', ctrlDeleteItem);
